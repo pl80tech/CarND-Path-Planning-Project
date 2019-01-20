61f32b5a29029b48f6f23357fa1e73a03f76639c
pl80tech <pl80.tech@gmail.com>
2019-01-20 14:35:08 +0900

    Quiz on lesson 10 - 14

---
+++ test_code/L10/14/cost.cpp
@@ -0,0 +1,20 @@
+#include <functional>
+#include <iostream>
+#include "cost.h"
+#include "cmath"
+
+
+using namespace std;
+
+float goal_distance_cost(int goal_lane, int intended_lane, int final_lane, float distance_to_goal) {
+    /*
+    The cost increases with both the distance of intended lane from the goal
+    and the distance of the final lane from the goal. The cost of being out of the 
+    goal lane also becomes larger as vehicle approaches the goal.
+    */
+    
+    //TODO: Replace cost = 0 with an appropriate cost function.
+    float cost = 0;
+    
+    return cost;
+}
