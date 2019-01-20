9681095af174b5ae0bb7016d2730d3ec93672514
pl80tech <pl80.tech@gmail.com>
2019-01-20 14:41:16 +0900

    Quiz on lesson 10 - 16

---
+++ test_code/L10/16/cost.cpp
@@ -0,0 +1,18 @@
+#include <functional>
+#include <iostream>
+#include "cost.h"
+#include "cmath"
+
+
+using namespace std;
+
+float inefficiency_cost(int target_speed, int intended_lane, int final_lane, vector<int> lane_speeds) {
+    /*
+    Cost becomes higher for trajectories with intended lane and final lane that have traffic slower than target_speed.
+    */
+    
+    //TODO: Replace cost = 0 with an appropriate cost function.
+    float cost = 0;
+    
+    return cost;
+}
