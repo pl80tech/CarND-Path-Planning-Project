9a9dd937076c0ced8cbde1d674bd59901dc14870
pl80tech <pl80.tech@gmail.com>
2019-01-22 07:05:58 +0900

    Add note to use the script

--- test_code/runtest.sh
+++ test_code/runtest.sh
@@ -1,3 +1,8 @@
 #!/bin/sh
 
+# Script to compile targeted source codes and run the compiled binary.
+# The command supports upto 4 source files as arguments.
+#
+# $./runtest.sh source1.cpp source2.cpp source3.cpp source4.cpp
+
 g++ $1 $2 $3 $4 -std=c++11 && ./a.out
