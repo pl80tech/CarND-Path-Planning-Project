c7ec2fc9a56f6173ab1470e1ea8b487c19bf2e89
pl80tech <pl80.tech@gmail.com>
2019-01-29 07:04:03 +0900

    Solution for quiz on lesson 10 - 16

--- test_code/L10/16/cost.cpp
+++ test_code/L10/16/cost.cpp
@@ -12,7 +12,9 @@ float inefficiency_cost(int target_speed, int intended_lane, int final_lane, vec
     */
     
     //TODO: Replace cost = 0 with an appropriate cost function.
-    float cost = 0;
+    float speed_intended = lane_speeds[intended_lane];
+    float speed_final = lane_speeds[final_lane];
+    float cost = (2.0*target_speed - speed_intended - speed_final)/target_speed;
     
     return cost;
 }
