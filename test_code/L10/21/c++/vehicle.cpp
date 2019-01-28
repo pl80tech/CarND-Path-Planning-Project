67f9f42481455ee417d22630e3570bc956022eff
pl80tech <pl80.tech@gmail.com>
2019-01-29 07:06:06 +0900

    Solution for quiz on lesson 10 - 21

--- test_code/L10/21/c++/vehicle.cpp
+++ test_code/L10/21/c++/vehicle.cpp
@@ -50,9 +50,25 @@ vector<Vehicle> Vehicle::choose_next_state(map<int, vector<Vehicle>> predictions
     */
     
     //TODO: Your solution here.
-    
+    vector<string> states = successor_states();
+    float cost;
+    vector<float> costs;
+    vector<vector<Vehicle>> final_trajectories;
+
+    for (vector<string>::iterator it = states.begin(); it != states.end(); ++it) {
+        vector<Vehicle> trajectory = generate_trajectory(*it, predictions);
+        if (trajectory.size() != 0) {
+            cost = calculate_cost(*this, predictions, trajectory);
+            costs.push_back(cost);
+            final_trajectories.push_back(trajectory);
+        }
+    }
+
+    vector<float>::iterator best_cost = min_element(begin(costs), end(costs));
+    int best_idx = distance(begin(costs), best_cost);
+
     //TODO: Change return value here:
-    return generate_trajectory("KL", predictions);
+    return final_trajectories[best_idx];
 }
 
 vector<string> Vehicle::successor_states() {
