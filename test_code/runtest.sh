416b77cc0d78fa1514474d260bb120fdea1032a3
pl80tech <pl80.tech@gmail.com>
2019-02-05 21:51:31 +0900

    Add description to use DEBUG definition flag

--- test_code/runtest.sh
+++ test_code/runtest.sh
@@ -1,8 +1,11 @@
 #!/bin/sh
 
 # Script to compile targeted source codes and run the compiled binary.
-# The command supports upto 4 source files as arguments.
+# The command supports upto 5 source files and compile flags as arguments.
 #
 # $./runtest.sh source1.cpp source2.cpp source3.cpp source4.cpp
+#
+# Add -DDEBUG like below to enable debug messages
+# $./runtest.sh source1.cpp source2.cpp source3.cpp source4.cpp -DDEBUG
 
-g++ $1 $2 $3 $4 -std=c++11 && ./a.out
+g++ $1 $2 $3 $4 $5 -std=c++11 && ./a.out
